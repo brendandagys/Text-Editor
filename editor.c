fc488b51694af0848b65a6e5bdcd7e93822de724
Brendan Dagys <brendandagys@gmail.com>
2024-09-07 13:42:28 -0400

    Support Delete key

--- editor.c
+++ editor.c
@@ -20,6 +20,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -87,35 +88,27 @@ int editorReadKey(void) {
         if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
-            case '1':
-              return HOME_KEY;
-            case '4':
-              return END_KEY;
-            case '5':
-              return PAGE_UP;
-            case '6':
-              return PAGE_DOWN;
-            case '7':
-              return HOME_KEY;
-            case '8':
-              return END_KEY;
+              // clang-format off
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+              // clang-format on
           }
         }
       } else {
         switch (seq[1]) {
-          // Arrow keys: '\x1b' + '[' + 'A|B|C|D'
-          case 'A':
-            return ARROW_UP;
-          case 'B':
-            return ARROW_DOWN;
-          case 'C':
-            return ARROW_RIGHT;
-          case 'D':
-            return ARROW_LEFT;
-          case 'H':
-            return HOME_KEY;
-          case 'F':
-            return END_KEY;
+            // clang-format off
+          case 'A': return ARROW_UP; // Arrow keys: '\x1b' + '[' + 'A|B|C|D'
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+            // clang-format on
         }
       }
     } else if (seq[0] == 'O') {
