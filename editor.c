4064dbca18cf8798ff3ba7f7921e17ae236338f1
Brendan Dagys <brendandagys@gmail.com>
2024-09-07 21:11:02 -0400

    Limit scrolling to the right

--- editor.c
+++ editor.c
@@ -303,12 +303,14 @@ void editorRefreshScreen(void) {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
       // clang-format off
-    case ARROW_LEFT:  if (E.cx != 0)        E.cx--; break;
-    case ARROW_RIGHT:                       E.cx++; break;
-    case ARROW_UP:    if (E.cy != 0)        E.cy--; break;
-    case ARROW_DOWN:  if (E.cy < E.numrows) E.cy++; break;
+    case ARROW_LEFT:  if (E.cx != 0)               E.cx--; break;
+    case ARROW_RIGHT: if (row && E.cx < row->size) E.cx++; break;
+    case ARROW_UP:    if (E.cy != 0)               E.cy--; break;
+    case ARROW_DOWN:  if (E.cy < E.numrows)        E.cy++; break;
       // clang-format on
   }
 }
