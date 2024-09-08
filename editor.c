59584aeca73279d80935f440623bcc3c59d37ee9
Brendan Dagys <brendandagys@gmail.com>
2024-09-08 12:07:30 -0400

    Page Up and Page Down now move cursor to top/bottom of screen and simulate a screen's worth of up-/down-arrow key presses

--- editor.c
+++ editor.c
@@ -394,6 +394,13 @@ void editorProcessKeypress(void) {
 
     case PAGE_UP:
     case PAGE_DOWN: {
+      if (c == PAGE_UP) {
+        E.cy = E.rowoff;
+      } else if (c == PAGE_DOWN) {
+        E.cy = E.rowoff + E.screenrows - 1;
+        if (E.cy > E.numrows) E.cy = E.numrows;
+      }
+
       int times = E.screenrows;  // Block allows declaration of `times` local variable
       while (times--)
         editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
