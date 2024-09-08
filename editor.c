f429340acdefe9dbb10099425571dca19cbcab3b
Brendan Dagys <brendandagys@gmail.com>
2024-09-07 21:15:59 -0400

    Go to end of previous line when moving cursor left at start of a line other than the first one

--- editor.c
+++ editor.c
@@ -307,7 +307,13 @@ void editorMoveCursor(int key) {
 
   switch (key) {
       // clang-format off
-    case ARROW_LEFT:  if (E.cx != 0)               E.cx--; break;
+    case ARROW_LEFT:
+      if (E.cx != 0) E.cx--;
+      else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
+      }
+      break;
     case ARROW_RIGHT: if (row && E.cx < row->size) E.cx++; break;
     case ARROW_UP:    if (E.cy != 0)               E.cy--; break;
     case ARROW_DOWN:  if (E.cy < E.numrows)        E.cy++; break;
