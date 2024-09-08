1996fc7afc9244bc681f19eb736de36352b958b5
Brendan Dagys <brendandagys@gmail.com>
2024-09-08 18:30:57 -0400

    Suppress input of special characters/key combinations

--- editor.c
+++ editor.c
@@ -25,6 +25,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -454,6 +455,8 @@ void editorProcessKeypress(void) {
 
   switch (c) {
       // clang-format off
+    case '\r': break;
+
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -463,6 +466,8 @@ void editorProcessKeypress(void) {
     case END_KEY:
       if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
       break;
+
+    case BACKSPACE: case DEL_KEY: case CTRL_KEY('h'): break;
 
     case PAGE_UP: case PAGE_DOWN: {
       if (c == PAGE_UP) {
@@ -479,7 +484,9 @@ void editorProcessKeypress(void) {
     case ARROW_UP: case ARROW_DOWN: case ARROW_LEFT: case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
-    
+
+    case CTRL_KEY('l'): case '\x1b': break;
+
     default: editorInsertChar(c); break;
       // clang-format on
   }
