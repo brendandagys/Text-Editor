b27f72314213e0b5f2dbbed0ca84cb5238aefa0c
Brendan Dagys <brendandagys@gmail.com>
2024-09-10 18:23:47 -0400

    Ctrl + s now saves the file with the same name that was opened Implemented basic error handling during save process Confirm save to user using a status message

--- editor.c
+++ editor.c
@@ -7,6 +7,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -60,6 +61,10 @@ struct editorConfig {
 };
 
 struct editorConfig E;
+
+/*** prototypes ***/
+
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -253,6 +258,24 @@ void editorInsertChar(int c) {
 
 /*** file i/o ***/
 
+char *editorRowsToString(int *buflen) {
+  int totlen = 0;
+  int j;
+  for (j = 0; j < E.numrows; j++) totlen += E.row[j].size + 1;
+  *buflen = totlen;
+
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+
+  return buf;
+}
+
 void editorOpen(char *filename) {
   free(E.filename);  // Free memory pointed to before reassigning with pointer from `strdup()`
   E.filename = strdup(filename);
@@ -273,6 +296,29 @@ void editorOpen(char *filename) {
 
   free(line);
   fclose(fp);
+}
+
+void editorSave(void) {
+  if (E.filename == NULL) return;
+
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        editorSetStatusMessage("%d bytes written to disk", len);
+        return;
+      }
+    }
+    close(fd);
+  }
+
+  free(buf);
+  editorSetStatusMessage("Can't save. I/O error: %s", strerror(errno));
 }
 
 /*** append buffer ***/
@@ -462,6 +508,10 @@ void editorProcessKeypress(void) {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
 
+    case CTRL_KEY('s'):
+      editorSave();
+      break;
+
     case HOME_KEY: E.cx = 0; break;
     case END_KEY:
       if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
@@ -510,7 +560,7 @@ int main(int argc, char *argv[]) {
   initEditor();
   if (argc >= 2) editorOpen(argv[1]);
 
-  editorSetStatusMessage("HELP: Ctrl-Q = quit");
+  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
   while (1) {
     editorRefreshScreen();
