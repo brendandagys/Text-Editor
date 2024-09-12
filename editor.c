0ff7ed5d01871e06828e82c3d3822cb2ae33d7b8
Brendan Dagys <brendandagys@gmail.com>
2024-09-11 20:26:50 -0400

    Search forward and backward

--- editor.c
+++ editor.c
@@ -416,15 +416,42 @@ void editorSave(void) {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
   // Return immediately if user pressed Esc or Enter to leave search mode
-  if (key == '\r' || key == '\x1b') return;
+  if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
+    return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
+  }
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
 
   for (int i = 0; i < E.numrows; i++) {
-    erow *row = &E.row[i];
+    current += direction;
+
+    // Cycle from bottom of file to top, or vice versa
+    if (current == -1) {
+      current = E.numrows - 1;
+    } else if (current == E.numrows) {
+      current = 0;
+    }
+
+    erow *row = &E.row[current];
     char *match = strstr(row->render, query);
 
     if (match) {
-      E.cy = i;
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
       break;
@@ -438,7 +465,7 @@ void editorFind(void) {
   int saved_coloff = E.coloff;
   int saved_rowoff = E.rowoff;
 
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  char *query = editorPrompt("Search: %s (Use arrows to step, ESC/Enter to abort)", editorFindCallback);
 
   if (query) {
     free(query);
